d8a757031fe9fac2d293b8fd2b8f11b05648e6d6
nikesha77 <pavlenkomikita@gmail.com>
2017-02-25 14:16:56 +0200

    added script to make disco

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,24 @@ var ball = document.querySelector(".discoball");
 
 function goDisco() {
 	//do your disco magic here !
+	ball.setAttribute("style","background: linear-gradient(to left, #FF512F , #DD2476)");
+}
+function goDisco1(){
+	ball.setAttribute("style","background: linear-gradient(to left, #E55D87 , #5FC3E4)");
+}
+function goDisco2(){
+	ball.setAttribute("style","background: linear-gradient(to left, #3494E6 , #EC6EAD)");
+}
+function goDisco3(){
+	ball.setAttribute("style","background: linear-gradient(to left, #00c3ff , #ffff1c)");
+
+}
+function goDisco4(){
+	ball.setAttribute("style","background: linear-gradient(to left, #de6161 , #2657eb)");
 }
 
 setInterval(goDisco,300);
+setInterval(goDisco1,600);
+setInterval(goDisco2,900);
+setInterval(goDisco3,1200);
+setInterval(goDisco4,1500);
